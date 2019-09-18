7858c05f1cef845dbb576e1575120bb27e9d4c71
EricFerguson76 <turtle0888@gmail.com>
2019-09-18 15:18:01 -0500

    updates to array mothods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -482,7 +482,7 @@ console.log(ticketPriceTotal);
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
-let runnersCompanyName = runners.filter(runner => runner.company_name);
+let runnersCompanyName = runners.filter(runner => runner.company_name === "Lazzy");
 console.log(runnersCompanyName);
 // Problem 2
 let lastNamesAllCaps = runners.map(runner => runner.last_name.toUpperCase());
